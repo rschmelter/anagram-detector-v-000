52aecd6cb4954787e61bb2813173382b370f3333
Ryan Schmelter <rschmelter33@gmail.com>
2017-11-16 00:37:15 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word    
+  end
+
+  def match(array)
+    new_array = []
+    array.each do |word|
+      if word.split.sort == @word.sort
+        new_array << word
+      else
+        []
+      end
+    end
+      new_array
+  end
+
+end
